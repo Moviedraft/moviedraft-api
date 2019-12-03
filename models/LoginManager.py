3906cc6957aaf0eedb99ebb211ffed83c567794d
Jason Kolenosky <jasonk58@gmail.com>
2019-12-03 11:22:33 -0800

    Moved LoginManager out of application.py

---
+++ models/LoginManager.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  3 11:10:32 2019
+
+@author: Jason
+"""
+
+from flask_login import LoginManager
+from models.Database import mongo
+from models.User import User
+
+login = LoginManager()
+
+@login.user_loader
+def load_user(username):
+    user = mongo.db.users.find_one({'username': username})
+    if not user:
+        return None
+    return User(
+            username=user['username'],
+            name = user['name'],
+            email = user['emailAddress'],
+            profilePic = user['picture']
+            )
