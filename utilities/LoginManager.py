248068cbbb15f8545104b4e1b77da4fcccedaad3
Jason Kolenosky <jasonk58@gmail.com>
2020-01-13 13:13:41 -0800

    Added userHandle field and stored session userId as actual userId

--- utilities/LoginManager.py
+++ utilities/LoginManager.py
@@ -6,21 +6,23 @@ Created on Tue Dec  3 11:10:32 2019
 """
 
 from flask_login import LoginManager
+from bson.objectid import ObjectId
 from utilities.Database import mongo
 from models.UserModel import UserModel
 
 login = LoginManager()
 
 @login.user_loader
-def load_user(username):
-    user = mongo.db.users.find_one({'username': username})
+def load_user(id):
+    print(id)
+    user = mongo.db.users.find_one({'_id': ObjectId(id)})
     if not user:
         return None
-    return UserModel(
-            username=user['username'],
+    return UserModel(id=str(user['_id']),
+            userHandle=user['userHandle'],
             firstName = user['firstName'],
             lastName = user['lastName'],
             email = user['emailAddress'],
-            profilePic = user['picture'],
+            picture = user['picture'],
             role = user['role']
             )
