dcde8fd85f5b090e90617d8c132d46fc807ee269
Jason Kolenosky <jasonk58@gmail.com>
2020-01-20 12:23:54 -0800

    Made user notification better and created user lookup logic

--- models/UserModel.py
+++ models/UserModel.py
@@ -5,6 +5,8 @@ Created on Tue Nov 19 10:03:06 2019
 @author: Jason
 """
 
+from utilities.Database import mongo
+
 class UserModel():
     def __init__(self, id, userHandle, firstName, lastName, email, picture, role):
         self.id = id
@@ -26,9 +28,32 @@ class UserModel():
     @staticmethod
     def is_anonymous():
         return False
-    
+        
     def get_id(self):
         return self.id
-
+    
     def allowed(self, requiredRole):
         return self.role >= requiredRole
+    
+    @classmethod
+    def load_user_by_email(cls, email):
+        queryDict = {'emailAddress': email.lower()}
+        user = cls.load_user(queryDict)  
+        return user
+    
+    @classmethod
+    def load_user(cls, queryDict):
+        user = mongo.db.users.find_one(queryDict)
+        if not user:
+            return None
+        return UserModel(
+                id = str(user['_id']),
+                userHandle = user['userHandle'],
+                firstName = user['firstName'],
+                lastName = user['lastName'],
+                email = user['emailAddress'],
+                picture = user['picture'],
+                role = user['role']
+                )
+    
+    
