be0969b5bf93177c41f133ec6796c7558d63459c
Jason Kolenosky <jasonk58@gmail.com>
2020-06-02 13:50:42 -0700

    Fixed poster scraper from using incorrect poster image

--- moviePosterScraper.py
+++ moviePosterScraper.py
@@ -21,7 +21,7 @@ movies = db.movies.find({}, {'title':1, 'releaseDate':1, '_id':1})
 for movie in movies:
     print('Updating \'{}\''.format(movie['title']))
     url = movieSearchUrl + movie['title']
-    response = requests.get(url = url) 
+    response = requests.get(url = url)
     data = response.json()
     if 'results' not in data:
         print('Could not find any results for \'{}\''.format(movie['title']))
@@ -32,13 +32,13 @@ for movie in movies:
             continue
         releaseYear = result['release_date'].split('-')[0]
         movieReleaseYear = movie['releaseDate'].strftime("%Y")
-        if releaseYear == movieReleaseYear:
+        if releaseYear == movieReleaseYear and movie['title'] in result['title']:
             posterPath = result['poster_path']
             if not posterPath:
                 print('No poster available for this \'{}\' result'.format(movie['title']))
                 continue
             posterUrl = moviePosterUrl + posterPath
-            db.movies.update_one({'_id': movie['_id']}, {'$set': {'posterUrl': posterUrl, 
+            db.movies.update_one({'_id': movie['_id']}, {'$set': {'posterUrl': posterUrl,
                                   'lastUpdated': datetime.strptime(datetime.today().isoformat() , '%Y-%m-%dT%H:%M:%S.%f')}})
             print('Updated \'{}\' with movie poster'.format(movie['title']))
             break
