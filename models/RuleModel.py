3b21f94c9d0fbe811973b5835e45d0da9453a7ee
Jason Kolenosky <jasonk58@gmail.com>
2020-07-06 15:26:37 -0700

    Apply grossCap rule

--- models/RuleModel.py
+++ models/RuleModel.py
@@ -11,6 +11,14 @@ class RuleModel():
     def __init__(self, ruleName, rules):
         self.ruleName = ruleName
         self.rules = rules
+
+    @classmethod
+    def apply_gross_cap(cls, gross_total, cap_value, cents_on_dollar):
+        if gross_total > cap_value:
+            total_to_manipulate = gross_total - cap_value
+            return round(total_to_manipulate * cents_on_dollar) + cap_value
+
+        return gross_total
         
     def load_rule(ruleName):
         rule = mongo.db.rules.find_one({'ruleName': ruleName})
