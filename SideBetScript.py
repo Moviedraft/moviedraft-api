910376924d93e3ed764f69a8706434750f698d30
Jason Kolenosky <jasonk58@gmail.com>
2020-09-15 11:02:30 -0700

    Update weekend_gross and fix single bid bets

--- SideBetScript.py
+++ SideBetScript.py
@@ -30,21 +30,25 @@ side_bets = db.sidebets.find({'status': SideBetStatus.current.value})
 side_bets_list = [side_bet for side_bet in side_bets]
 
 for side_bet in side_bets_list:
+    movie = db.movies.find_one({'_id': side_bet['movie_id']})
+
     if len(side_bet['bets']) < 1:
         print('No bets made for side bet ID: \'{}\''.format(side_bet['_id']))
         continue
-
-    movie = db.movies.find_one({'_id': side_bet['movie_id']})
-    winning_bet = max((side_bet for side_bet in side_bet['bets'] if movie['domesticGross'] - side_bet['bet'] > 0), key= lambda x: x['bet'])
+    elif len(side_bet['bets']) == 1:
+        winning_bet = side_bet['bets'][0] if side_bet['bets'][0]['bet'] < movie['domesticGross'] else {'user_id': None, 'bet': None}
+    else:
+        winning_bet = max((side_bet for side_bet in side_bet['bets'] if movie['domesticGross'] - side_bet['bet'] > 0), key= lambda x: x['bet'])
 
     print('Updating side bet ID: \'{}\' with winner \'{}\'. Bet was $\'{}\'. Weekend gross was $\'{}\''
           .format(side_bet['_id'],
-                  winning_bet['user_id'],
+                  winning_bet['user_id'] or None,
                   winning_bet['bet'],
                   movie['domesticGross']))
 
     db.sidebets.update_one({'_id': side_bet['_id']},
                            {'$set': {'winner': ObjectId(winning_bet['user_id']),
-                                     'status': SideBetStatus.previous.value }})
+                                     'status': SideBetStatus.previous.value,
+                                     'weekend_gross': movie['domesticGross']}})
 
     print('Updated side bet ID: \'{}\''.format((side_bet['_id'])))
